ea4fbd1bfea0680c4d0d70e85b19e4f1bb17e862
OlgaM <139258143+malaniak96@users.noreply.github.com>
2024-01-12 23:30:15 +0100

    lecture 3 finish

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -1,6 +1,6 @@
-import {config} from "dotenv";
+import { config } from "dotenv";
+
 config();
-export const configs =
-{
-    PORT: process.env.PORT
-}
+export const configs = {
+  PORT: process.env.PORT,
+};
--- src/fs.service.ts
+++ src/fs.service.ts
@@ -1,22 +1,22 @@
-import fs from 'fs/promises';
-import path from 'path';
+import fs from "fs/promises";
+import path from "path";
 
 interface IUser {
-    id: number,
-    age: number,
-    name: string,
-    email: string
+  id: number;
+  age: number;
+  name: string;
+  email: string;
 }
 
-const pathToFile = path.join(process.cwd(), 'db.json');
+const pathToFile = path.join(process.cwd(), "db.json");
 
 export const read = async (): Promise<IUser[]> => {
-    const json = await fs.readFile(pathToFile, {encoding: 'utf-8'});
-    return JSON.parse(json);
-}
+  const json = await fs.readFile(pathToFile, { encoding: "utf-8" });
+  return JSON.parse(json);
+};
 
 export const write = async (users) => {
-    await fs.writeFile(pathToFile, JSON.stringify(users));
-}
+  await fs.writeFile(pathToFile, JSON.stringify(users));
+};
 
-module.exports = {read, write}
+module.exports = { read, write };
